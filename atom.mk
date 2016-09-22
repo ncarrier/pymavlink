5e39cc62042c055ee15baf4fb83dd46889892ca4
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-09-22 10:07:35 +0200

    [BUILD] correct atom.mk
    * set a category
    * don't look for message definition xml files
    * install in staging-host/usr/lib/pymavlink/mavgen, not .../lib/mavgen
    
    Change-Id: I313444270547f7f5ac495158f0de538b026ed090

--- atom.mk
+++ atom.mk
@@ -3,23 +3,23 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_HOST_MODULE := mavgen
+LOCAL_CATEGORY_PATH := mavlink
 
 mavgen_files := \
 	$(call all-files-under,.,.py) \
 	$(call all-files-under,.,.xsd) \
-	$(call all-files-under,.,.h) \
-	$(call all-files-under,message_definitions,.xml)
+	$(call all-files-under,.,.h)
 
 # Install files in host staging directory
 LOCAL_COPY_FILES := \
 	$(foreach __f,$(mavgen_files), \
-		$(__f):$(HOST_OUT_STAGING)/usr/lib/mavgen/$(__f) \
+		$(__f):$(HOST_OUT_STAGING)/usr/lib/mavgen/pymavlink/$(__f) \
 	)
 
 # Needed to force a build order of LOCAL_COPY_FILES
 LOCAL_EXPORT_PREREQUISITES := \
 	$(foreach __f,$(mavgen_files), \
-		$(HOST_OUT_STAGING)/usr/lib/mavgen/$(__f) \
+		$(HOST_OUT_STAGING)/usr/lib/mavgen/pymavlink/$(__f) \
 	)
 
 include $(BUILD_CUSTOM)
